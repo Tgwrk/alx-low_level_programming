b9f81ba9755bcc2fd17456366ab8dd92c380c7c3
Tigist <tigiworku5@gmail.com>
2022-06-06 12:01:49 +0000

    Combinations of two numbers

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,9 +10,9 @@ int main(void)
 {
 	int num1,num2;
 
-	for (num1 = '0'; num1 <= '9'; num++)
+	for (num1 = '0'; num1 <= '9'; num1++)
 	{
-		for (num2 =num1 + 1; num2<= '9'; num2++)
+		for (num2 = num1 + 1; num2 <= '9'; num2++)
 		{
 			putchar(num1);
 			putchar(num2);
