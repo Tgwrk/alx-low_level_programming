266d2887d07f9b4a71ebe838dc4936c8264b4eeb
Tigist <tigiworku5@gmail.com>
2022-06-06 10:38:58 +0000

    Alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Print alphabet letters
+ *
+ * Return:0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 122; i++)
+	{
+		putchar(i);
+	
+	}
+	putchar('\n');
+	return (0);
+}
