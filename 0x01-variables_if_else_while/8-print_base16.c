61b5b6bf81d6dd5a36119722b72f8b3a2c05689f
Tigist <tigiworku5@gmail.com>
2022-06-06 11:41:51 +0000

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints all the number of base 16 in lowercase
+ *
+ * Return:0
+ */
+int main(void)
+{
+	int i;
+
+	for (i=61; i <= 79; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+
+
+}
