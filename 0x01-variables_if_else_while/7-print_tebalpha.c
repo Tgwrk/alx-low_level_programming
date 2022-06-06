8981c003b0addd25c1b656199035528a3c430052
Tigist <tigiworku5@gmail.com>
2022-06-06 06:12:22 +0000

    Prints all the lowercase alphabets in reverse order

---
+++ 0x01-variables_if_else_while/7-print_tebalpha.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints the lowercase alphabets in reverse order
+ *
+ * Return : 0
+ */
+int main(void)
+{
+	int alphabet;
+
+	for (alphabet = 'z'; alphabet >= 'a'; alphabet--)
+	{
+		putchar(alphabet);
+		putchar('\n');
+	}
+	return (0);
+}
