176c36128366a1637946f2f31d6cd5fba3017ddc
Tigist <tigiworku5@gmail.com>
2022-06-08 13:15:29 +0000

    Print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main function
+ *
+ * Return:0
+ */
+
+int main(void)
+{
+	char string[8] = "_putchar";
+	int n = 0;
+
+	while (n < 8)
+	{
+		_putchar(string[n]);
+		n++;
+	}
+
+	_putchar(10);
+	return (0);
+}
