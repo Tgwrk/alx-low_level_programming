5affc7ecac6485c37637679103238368a66f78ad
Tigist <tigiworku5@gmail.com>
2022-06-08 13:46:53 +0000

    Print the alphabet in lowercase x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the lowercase alphabets
+ *
+ * Return:0
+ */
+
+void print_alphabet_x10(void)
+{
+	int j;
+	int x;
+
+	for (x = 0; x <= 'z'; x++)
+	{
+		for (j = 'a'; j <= 'Z'; j++)
+		{
+			_putchar(j);
+		}
+	}
+
+	 _putchar('\n');
+}
