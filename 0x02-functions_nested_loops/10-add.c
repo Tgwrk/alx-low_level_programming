3d9ef7d3eef7cd6917436c143300392bc8d614f3
Tigist <tigiworku5@gmail.com>
2022-06-08 19:36:47 +0000

    Print the last digit of a number

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * add - finds the sum of two numbers
+ * @num1 - parameter
+ * @num2 - parameter
+ * Return:0
+ */
+
+int add(int num1, int num2)
+{
+	int num1, num2;
+
+	_putchar(num1);
+	_putchar(num2);
+	add = num1 + num2;
+
+	_putchar(add);
+	_putchar('\n');
+}
